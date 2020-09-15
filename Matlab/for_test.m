4ba3433193641f9b8cbe8ce10c8ebb67f9ce6316
Ekarat Sathitwattanasan <ekekarat.stw@gmail.com>
2020-09-15 15:50:59 +0700

    clean files.

--- Matlab/for_test.m
+++
@@ -1,5 +0,0 @@
-A = [3 6 9 4 1];
-for i = 1:length(A)
-       disp(A(i))
-       disp(i)
-end
--- Matlab/main_simple1.m
+++
@@ -1,6 +0,0 @@
-t_span = [0 12];      %Time period
-Y_init = [  0 
-            1 
-            1];     %Initial condition
-[T1,Y1] = ode45(@simple1,t_span,Y_init);
-plot(T1,Y1(:,1),'-', T1,Y1(:,2),'-.', T1,Y1(:,3),':');
